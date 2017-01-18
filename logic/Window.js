3901e80310fe2d465f0069ac136abdcec230f1f7
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2017-01-18 21:21:27 +0000

    feat: added class for storing windows of labels
    Will store the ID of the log node that had a soft lockup, the sequence of labels within the 3 hour window and the window's label.
    
    Daniel

---
+++ logic/Window.js
@@ -0,0 +1,11 @@
+class Window {
+
+  constructor(nodeId, sequenceOfLabels, label) {
+    this.nodeId = nodeId;
+    this.sequenceOfLabels = sequenceOfLabels;
+    this.label = label;
+  }
+  
+}
+
+module.exports = Window;
