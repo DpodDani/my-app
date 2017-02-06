3b4b9583a8fddef10692df33b553cd383ade2f6b
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2017-02-06 21:53:32 +0000

    refactor: added line number attribute
    This will be useful for locating particular log lines in the log files, after the hashmap has been cleaned (and maybe restructured).
    
    Daniel

--- logic/LogNode.js
+++ logic/LogNode.js
@@ -3,17 +3,24 @@ const moment = require('moment');
 /**
  *  This class will act as a container for each log line that is read from the log file.
  *
- *  @param  {Integer} id        Unique number used to identify the LogNode object.
- *  @param  {String}  message   The log line read from the log file.
- *  @param  {Moment}  timestamp The timestamp extracted from the log line.
+ *  @param  {Integer}   lineNo   The line number of the log message in the log file.
+ *  @param  {String}    message   The log line read from the log file.
+ *  @param  {Moment}    timestamp The timestamp extracted from the log line.
+ *  @param  {Integer}   jobId     The number representing the process job ID
+ *  @param  {Character} label     The sentiment of a particular log line.
  */
 class LogNode {
 
-  constructor(message, timestamp, jobId, label) {
+  constructor(lineNo, message, timestamp, jobId, label) {
+    this.lineNo = lineNo;
     this.message = message;
     this.timestamp = timestamp;
     this.jobId = jobId;
     this.label = label;
+  }
+
+  getLineNum() {
+    return this.lineNo;
   }
 
   getTimestamp() {
