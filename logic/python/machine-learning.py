23564a373ab9b476b4a2bd7e41b18c8ab2041f26
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2017-02-19 20:11:50 +0000

    feat: using the Decision Tree model from sklearn
    Decided to use the machine learning models offered by an open source Python module. Currently am testing the Decision Tree model, and it seems to be really good. It currently reads from a CSV file (created by nodeJS) that contains the testing data and their respective labels.
    
    Daniel

---
+++ logic/python/machine-learning.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import os
+from sklearn import preprocessing, tree
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+clf = tree.DecisionTreeClassifier()
+
+def main():
+    log_windows = pd.read_csv(dirname + '/window_attr.csv', header = 0)
+    # Standardise the data
+    scalar_windows = preprocessing.StandardScaler().fit(log_windows)
+    scaled_windows = scalar_windows.transform(log_windows)
+
+    # Obtain labels and delete it from log window DataFrame
+    window_labels = log_windows['label']
+    del log_windows['label']
+
+    # Fitting the model
+    clf.fit(log_windows, window_labels)
+
+    # Obtaining attributes from training dataset (for testing)
+    index = 11
+    b = log_windows['noOfBs']
+    noOfBs = b[index]
+    g = log_windows['noOfGs']
+    noOfGs = g[index]
+    f = log_windows['noOfFs']
+    noOfFs = f[index]
+
+    # Predicting the class of a sample
+    result = clf.predict( [ [ noOfBs, noOfGs, noOfFs ] ] )
+
+    print log_windows['noOfBs'].describe(), "\n"
+
+    print ("Result: ", result)
+
+if __name__ == "__main__":
+    main()
