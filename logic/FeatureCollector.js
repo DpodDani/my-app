12f1fe0150c4dde00b47a0530d0ad358a2a55cd0
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2017-02-21 13:22:59 +0000

    feat: created feature collecting JS module
    The purpose of this file is to store all the features/attributes, for the log Windows, in a separate file. This makes it easier to identify and add/delete features when the need arises.
    
    Daniel

---
+++ logic/FeatureCollector.js
@@ -0,0 +1,68 @@
+// This will be called in an Async forEach scope
+// Will collect features for each logWindow that is passed down into it
+class FeatureCollector {
+
+  // This is the Window class
+  constructor(logWindow) {
+    this.logWindow = logWindow;
+    this.sequenceOfLabels = logWindow.getSequence();
+  }
+
+  startCollection() {
+    let logWindow = this.logWindow;
+
+    return new Promise( (resolve, reject) => {
+      // calls all the feature collection functions
+      logWindow.setNoOf('B', this.getLabelFreq('B'));
+      logWindow.setNoOf('G', this.getLabelFreq('G'));
+      logWindow.setNoOf('F', this.getLabelFreq('F'));
+      logWindow.setLongestSeqOf('B', this.getLongestSequence('B'));
+      logWindow.setLongestSeqOf('G', this.getLongestSequence('G'));
+
+      resolve(logWindow);
+    });
+
+  }
+
+  getLabelFreq(label) {
+    const logWindow = this.logWindow;
+    const sequenceOfLabels = this.sequenceOfLabels;
+    if (sequenceOfLabels.length > 0) {
+      switch(label){
+        case 'B': return (sequenceOfLabels.match(/B/g)) ? sequenceOfLabels.match(/B/g).length : 0; break;
+        case 'G': return (sequenceOfLabels.match(/G/g)) ? sequenceOfLabels.match(/G/g).length : 0; break;
+        case 'F': return (sequenceOfLabels.match(/F/g)) ? sequenceOfLabels.match(/F/g).length : 0; break;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  getLongestSequence(label) {
+    const sequenceOfLabels = this.sequenceOfLabels;
+    let searchForSequence = true;
+    let maxLength = 0;
+    let currentLength = 0;
+
+    for (let i = 0; i < sequenceOfLabels.length; i++){
+      if (!searchForSequence){
+        if (sequenceOfLabels[i] == label){
+          currentLength++;
+          searchForSequence = true;
+        }
+      } else {
+        if (sequenceOfLabels[i] == label){
+          currentLength++;
+        } else {
+          if (currentLength > maxLength) maxLength = currentLength;
+          currentLength = 0;
+          searchForSequence = false;
+        }
+      }
+    }
+    return maxLength;
+  }
+
+}
+
+module.exports = FeatureCollector;
