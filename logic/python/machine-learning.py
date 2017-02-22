a2b7607ec7b0b6cf2da4a871a5ddb2291ea73f38
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2017-02-22 22:25:25 +0000

    test: machine-learning: tested 3 different models for classification
    Testing using the Naive Bayes, SVM and Decision Tree Classifiers. Using the describe() function I was able to plot a normal distribution of the scores of each model from applying cross validation.
    
    Daniel

--- logic/python/machine-learning.py
+++ logic/python/machine-learning.py
@@ -1,38 +1,75 @@
 import pandas as pd
 import numpy as np
 import os
-from sklearn import preprocessing, tree
+from sklearn import preprocessing, tree, svm, naive_bayes
+from sklearn.model_selection import cross_val_score
 
 dirname = os.path.dirname(os.path.abspath(__file__))
-clf = tree.DecisionTreeClassifier()
 
 def main():
     log_windows = pd.read_csv(dirname + '/window_attr.csv', header = 0)
-    # Standardise the data
-    scalar_windows = preprocessing.StandardScaler().fit(log_windows)
-    scaled_windows = scalar_windows.transform(log_windows)
 
     # Obtain labels and delete it from log window DataFrame
     window_labels = log_windows['label']
     del log_windows['label']
 
+    # Standardise the data
+    scalar = preprocessing.StandardScaler().fit(log_windows)
+    scaled_windows = scalar.transform(log_windows)
+
+    # DEFINE MODEL
+    clf = svm.SVC()
+    #clf = tree.DecisionTreeClassifier()
+    #clf = naive_bayes.GaussianNB()
+
     # Fitting the model
-    clf.fit(log_windows, window_labels)
+    clf.fit(scaled_windows, window_labels)
+
+    # Cross validation score
+    scores = cross_val_score(clf, log_windows, window_labels, cv=10)
+    print ('Scores: ', scores)
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    #print('Importance ', clf.feature_importances_)
+
+    # 1056,592,1,1244,325
+    # 29315,120543,0,0,0
+    # 865,905,0,0,0
+    # 1512,1602,0,0,0
+    # 2085,1411,0,0,0
+    # 1126,3562,0,0,0
+    # 512,450,0,0,0
+    # 550,521,0,0,0
+    # 767,441,0,0,0
+    # 831,707,0,0,0
+    # 205,148,1,5,98
+    # 900,918,0,0,0
+    # 1010,1011,0,0,0
+    # 529,405,0,0,0
+    # 16604,91071,1,13185,1235
+    # 6248,51401,0,0,0
 
     # Obtaining attributes from training dataset (for testing)
-    index = 11
+    index = 19
     b = log_windows['noOfBs']
     noOfBs = b[index]
     g = log_windows['noOfGs']
     noOfGs = g[index]
     f = log_windows['noOfFs']
     noOfFs = f[index]
+    b2 = log_windows['longestSeqOfBs']
+    longestSeqOfBs = b2[index]
+    g2 = log_windows['longestSeqOfGs']
+    longestSeqOfGs = g2[index]
 
     # Predicting the class of a sample
-    result = clf.predict( [ [ noOfBs, 1234567687, 0 ] ] )
+    new_values = [[1056,592,1,1244,325]]
+    scaled_new_values = scalar.transform(new_values)
+    result = clf.predict(scaled_new_values)
 
     print log_windows['noOfBs'].describe(), "\n"
-
+    print "Unscaled new values: ", new_values
+    print "Scaled new values: ", (scaled_new_values, "\n")
     print ("Result: ", result)
 
 if __name__ == "__main__":
