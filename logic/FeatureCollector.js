7301ca9fcfce56d5fcea74f30bb03fbb76eeedd1
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2017-04-22 10:24:25 +0100

    refactor: added 2 new features to FeatureCollector.js
    Closest longest sequence of Gs to F (as well as one for Bs).
    
    Daniel

--- logic/FeatureCollector.js
+++ logic/FeatureCollector.js
@@ -3,21 +3,28 @@
 class FeatureCollector {
 
   // This is the Window class
-  constructor(logWindow) {
+  constructor(logWindow, logWindowId) {
     this.logWindow = logWindow;
     this.sequenceOfLabels = logWindow.getSequence();
+    this.logWindowId = logWindowId;
   }
 
   startCollection() {
     let logWindow = this.logWindow;
 
+    // TODO: Get all subsequences from Window and label them with an "order"
+    // Track their length as well
     return new Promise( (resolve, reject) => {
       // calls all the feature collection functions
+      const bSubsequencesInfo = this.getSubsequencesInfo('B');
+      const gSubsequencesInfo = this.getSubsequencesInfo('G');
       logWindow.setFeature('noOfBs', this.getLabelFreq('B'));
       logWindow.setFeature('noOfGs', this.getLabelFreq('G'));
       logWindow.setFeature('noOfFs', this.getLabelFreq('F'));
-      logWindow.setFeature('longestSeqOfBs', this.getLongestSequence('B'));
-      logWindow.setFeature('longestSeqOfGs', this.getLongestSequence('G'));
+      logWindow.setFeature('longestSeqOfBsDistanceFromF', bSubsequencesInfo.longestSeqDistanceFromF);
+      logWindow.setFeature('longestSeqOfGsDistanceFromF', gSubsequencesInfo.longestSeqDistanceFromF);
+      logWindow.setFeature('closestSeqOfGsToF', gSubsequencesInfo.distanceOfClosestSeq);
+      logWindow.setFeature('closestSeqOfBsToF', bSubsequencesInfo.distanceOfClosestSeq);
 
       resolve(logWindow);
     });
@@ -38,12 +45,14 @@ class FeatureCollector {
     }
   }
 
-  getLongestSequence(label) {
+  getSubsequencesInfo(label) {
     const sequenceOfLabels = this.sequenceOfLabels;
     let searchForSequence = true;
     let indexOfMaxSeq = 0;
     let maxLength = 0;
     let currentLength = 0;
+    let foundClosestSeq = false;
+    let indexOfClosestSeq = 0;
 
     for (let i = 0; i < sequenceOfLabels.length; i++){
       if (!searchForSequence){
@@ -59,12 +68,22 @@ class FeatureCollector {
             maxLength = currentLength;
             indexOfMaxSeq = i - currentLength;
           }
+          if (!foundClosestSeq && currentLength > 1) { // TEST THIS
+            indexOfClosestSeq = i - currentLength;
+            foundClosestSeq = true;
+          }
           currentLength = 0;
           searchForSequence = false;
         }
       }
     }
-    return (this.getLabelFreq('F') > 0) ? indexOfMaxSeq : 0;
+
+    return {
+      "longestSeqDistanceFromF" : (sequenceOfLabels[0] === 'F') ? indexOfMaxSeq : 0,
+      "distanceOfClosestSeq" : (sequenceOfLabels[0] === 'F') ? indexOfClosestSeq : 0
+    }
+
+    //return (sequenceOfLabels[0] === 'F') ? indexOfMaxSeq : 0;
   }
 
 }
